23207e68c6a9f4be04aa811c1c5fa42dc8c46ddd
Eiji Shinohara <shinodogg@gmail.com>
2013-12-26 15:03:01 +0900

    hoge

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hoge::Application.config.secret_key_base = '5c3d8ab76006440f54405e8ac51a1ce0e1f1bcb7d00ea67f6d3c560d718bda0f0d52ecfedf2e694cb1a7bd34ba76e461c1ce44c293ae53b3404c0ef1ffd01a57'
